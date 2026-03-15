f99a1aeedf169962483ca4f4521b510291fadcf5
Ross Charles C. <10781734+rossbridger@users.noreply.github.com>
2026-03-16 00:30:37 +0800

    Further polish camera control.
    Signed-off-by: Ross Charles C. <10781734+rossbridger@users.noreply.github.com>

--- main.gd
+++ main.gd
@@ -1,1 +1,17 @@
 extends Node
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CONFINED
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("enter") \
+	|| Input.is_action_just_released("mouseclick_left"):
+		get_viewport().set_input_as_handled()
+		Input.mouse_mode = Input.MOUSE_MODE_CONFINED
+
+	if Input.is_action_just_released("esc"):
+		get_viewport().set_input_as_handled()
+		if Input.mouse_mode == Input.MOUSE_MODE_CONFINED:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		else:
+			get_tree().quit()
--- rts_camera.gd
+++ rts_camera.gd
@@ -10,9 +10,12 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	camera_pan(delta)
+	camera_move(delta)
 	_apply_movement_velocity()
 
 func camera_pan(delta: float) -> void:
+	if Input.mouse_mode != Input.MOUSE_MODE_CONFINED:
+		return
 	var mouse_pos = get_viewport().get_mouse_position()
 	var viewport_size = get_viewport().get_visible_rect().size
 	if mouse_pos.x < CAMERA_PAN_MARGIN:
@@ -23,6 +26,16 @@ func camera_pan(delta: float) -> void:
 		cam_movement_velocity.x = 1 * delta
 	if mouse_pos.y > viewport_size.y - CAMERA_PAN_MARGIN:
 		cam_movement_velocity.z = 1 * delta
+
+func camera_move(delta: float) -> void:
+	if Input.is_action_pressed("camera_forward"):
+		cam_movement_velocity.z = -1 * delta
+	if Input.is_action_pressed("camera_backward"):
+		cam_movement_velocity.z = 1 * delta
+	if Input.is_action_pressed("camera_left"):
+		cam_movement_velocity.x = -1 * delta
+	if Input.is_action_pressed("camera_right"):
+		cam_movement_velocity.x = 1 * delta
 
 func _setup_camera(cam: Camera3D) -> void:
 	cam.fov = 10
