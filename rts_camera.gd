5ed6d4997d94821a39eba4fba186a5ead7cbc4e8
Ross Charles C. <10781734+rossbridger@users.noreply.github.com>
2026-03-21 16:48:22 +0800

    Add Camera zooming and rotation.
    Signed-off-by: Ross Charles C. <10781734+rossbridger@users.noreply.github.com>

--- rts_camera.gd
+++ rts_camera.gd
@@ -1,9 +1,15 @@
 extends Node3D
 
-const CAMERA_PAN_MARGIN: float = 5.0 #pixels to trigger pan on screen edge
+const CAMERA_PAN_MARGIN := 5.0 #pixels to trigger pan on screen edge
+const CAMERA_ROTATION_SPEED := 1.0
+const CAMERA_ZOOM_SPEED := 4.0
+const CAMERA_ZOOM_RANGE := Vector2(50, 200)
+const CAMERA_MOVE_SPEED := Vector2(40, 100)
+var cam_movement_velocity := Vector3.ZERO
+var cam_zoom_velocity := 0.0
 
-var cam_movement_velocity = Vector3.ZERO
-@onready var camera_3d: Camera3D = $Camera3D
+
+@onready var camera_3d := $Camera3D
 
 func _ready() -> void:
 	_setup_camera(camera_3d)
@@ -11,7 +17,10 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	camera_pan(delta)
 	camera_move(delta)
+	camera_rotate(delta)
+	camera_zoom(delta)
 	_apply_movement_velocity()
+	_apply_zoom_velocity()
 
 func camera_pan(delta: float) -> void:
 	if Input.mouse_mode != Input.MOUSE_MODE_CONFINED:
@@ -37,6 +46,18 @@ func camera_move(delta: float) -> void:
 	if Input.is_action_pressed("camera_right"):
 		cam_movement_velocity.x = 1 * delta
 
+func camera_rotate(delta: float) -> void:
+	if Input.is_action_pressed("camera_rotate_right"):
+		global_rotation.y += CAMERA_ROTATION_SPEED * delta
+	if Input.is_action_pressed("camera_rotate_left"):
+		global_rotation.y -= CAMERA_ROTATION_SPEED * delta
+
+func camera_zoom(delta: float) -> void:
+	if Input.is_action_just_pressed("camera_zoom_in"):
+		cam_zoom_velocity -= (CAMERA_ZOOM_SPEED * 1000) * delta
+	if Input.is_action_just_pressed("camera_zoom_out"):
+		cam_zoom_velocity += (CAMERA_ZOOM_SPEED * 1000) * delta
+
 func _setup_camera(cam: Camera3D) -> void:
 	cam.fov = 10
 	cam.position.y = 3.0
@@ -46,5 +67,18 @@ func _setup_camera(cam: Camera3D) -> void:
 
 func _apply_movement_velocity():
 	if cam_movement_velocity != Vector3.ZERO:
-		translate_object_local(cam_movement_velocity * 40)
+		var camera_zoom_speed := remap(
+			camera_3d.position.z,
+			CAMERA_ZOOM_RANGE.x, CAMERA_ZOOM_RANGE.y,
+			CAMERA_MOVE_SPEED.x, CAMERA_MOVE_SPEED.y
+		)
+		translate_object_local(cam_movement_velocity * camera_zoom_speed)
 		cam_movement_velocity = Vector3.ZERO
+
+func _apply_zoom_velocity(cam: Camera3D = camera_3d) -> void:
+	if cam_zoom_velocity != 0:
+		var calculated_zoom := cam.position.z + cam_zoom_velocity
+		if calculated_zoom > CAMERA_ZOOM_RANGE.x \
+		&& calculated_zoom < CAMERA_ZOOM_RANGE.y:
+			cam.translate_object_local(Vector3(0, 0, cam_zoom_velocity))
+	cam_zoom_velocity = 0
